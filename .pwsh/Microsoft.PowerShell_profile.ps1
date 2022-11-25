e6214b4046fd62caba1bbd498a855a1d941fb009
CEbbinghaus <git@cebbinghaus.com>
2022-11-25 13:14:21 +1100

    Added Right prompt, Fixed error in non GUI environment

--- .pwsh/Microsoft.PowerShell_profile.ps1
+++ .pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,16 @@
-Invoke-Expression (&starship init powershell)
+Clear-Host *> $null
+
+# If Clear-Host fails we know that this is a non GUI environment so we don't need to load any of the profile.
+if(-not $?) {
+	return;
+}
+
+Invoke-Expression (&starship init powershell) -ErrorAction SilentlyContinue
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 if($PWD.Path -eq "C:\WINDOWS\system32") {
   Set-Location ~
 }
-
-
-Clear-Host
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -16,12 +20,12 @@ if (Test-Path($ChocolateyProfile)) {
 
 function ConvertMp4ToMkv {
   param (
-    [string]$FilePath
+	[string]$FilePath
   )
 
   if (-not(Test-Path -Path $FilePath -PathType Leaf)) {
-    Write-Host "File $FilePath doesn't exist";
-    return;
+	Write-Host "File $FilePath doesn't exist";
+	return;
   }
 
   $File = Get-Item $FilePath 
@@ -34,7 +38,7 @@ function ConvertMp4ToMkv {
 
 function config()
 {
-    &git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args
+	&git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args
 }
 
 function Reload {
@@ -42,8 +46,8 @@ function Reload {
 }
 
 function IsAdministrator {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+	$user = [Security.Principal.WindowsIdentity]::GetCurrent();
+	return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
 function Format-FileSize() {
@@ -61,3 +65,34 @@ function Format-FileSize() {
 
 # function Global:prompt {
 # }
+
+function Array-To-Object
+{
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory)]
+		# allow string arrays:
+		[string[]]
+		$Keys,
+		[Parameter(Mandatory,ValueFromPipeline)]
+		[object[]]
+		$Values
+	)
+	process
+	{
+		if($Values -isnot [System.Array])
+		{
+			Write-Error "Not a Array"
+			return $null
+		}
+		
+		$table = [ordered]@{}
+		
+		for($i = 0; $i -lt $Keys.Count -and $i -lt $Values.Count; ++$i)
+		{
+			$table.add($Keys[$i], $Values[$i]); 
+		}
+		
+		return [pscustomobject]$table
+	}
+}
