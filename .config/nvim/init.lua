335d5e268014405d84862b8a2474672a891a6de8
CEbbinghaus <git@cebbinghaus.com>
2023-05-14 03:33:08 +1000

    Finished move to lazy & got LSP set up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,14 +3,14 @@ vim.cmd.source(vimrc)
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,28 +19,48 @@ vim.g.mapleader = " "
 
 require("lazy").setup({
 	-- Theme
-	{"catppuccin/nvim", name = "catppuccin" },
+	{ "catppuccin/nvim",                 name = "catppuccin" },
 	-- Saves last location
 	"ethanholz/nvim-lastplace",
 	-- Syntax Highlighting
-	{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 	-- Mason is a automatic LSP,DAP,linter installer
 	-- :MasonUpdate updates registry contents:
-	{'williamboman/mason.nvim', build = ':MasonUpdate' },
+	{ 'williamboman/mason.nvim',         build = ':MasonUpdate' },
 	-- LSP config
-	'neovim/nvim-lspconfig',
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		dependencies = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{
+				-- Optional
+				'williamboman/mason.nvim',
+				build = function()
+					pcall(vim.cmd, 'MasonUpdate')
+				end,
+			},
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+		}
+	},
 
 	-- File Browser
 	'nvim-lua/plenary.nvim',
 	'nvim-tree/nvim-web-devicons',
 	'MunifTanjim/nui.nvim',
 	'nvim-neo-tree/neo-tree.nvim',
-	
+
 	-- Git blame per line
 	'f-person/git-blame.nvim',
 
-	-- Telescope fzf 
-	{'nvim-telescope/telescope.nvim', tag = '0.1.1' }
+	-- Telescope fzf
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.1' }
 })
 
 vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
@@ -52,3 +72,14 @@ vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
 
 vim.cmd.colorscheme('catppuccin-macchiato')
+
+local lsp = require('lsp-zero').preset({})
+
+-- Setting up lspconfig with lua_ls https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/tutorial.md
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({ buffer = bufnr })
+end)
+
+lsp.setup()
