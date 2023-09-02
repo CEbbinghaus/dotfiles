2537619caed5b1adea580e8138fcc032b0c2b2c6
CEbbinghaus <git@cebbinghaus.com>
2023-09-02 10:40:48 +0000

    Added NixOS configuration

---
+++ .config/nixos/configuration.nix
@@ -0,0 +1,43 @@
+{ lib, pkgs, config, modulesPath, ... }:
+
+with lib;
+let
+  nixos-wsl = import ./nixos-wsl;
+
+  nvim = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz";
+  }) {};
+
+  neovim = nvim.neovim;
+in
+{
+  imports = [
+    "${modulesPath}/profiles/minimal.nix"
+
+    nixos-wsl.nixosModules.wsl
+  ];
+
+  wsl = {
+    enable = true;
+    automountPath = "/mnt";
+    defaultUser = "nixos";
+    startMenuLaunchers = true;
+
+    # Enable native Docker support
+    # docker-native.enable = true;
+
+    # Enable integration with Docker Desktop (needs to be installed)
+    # docker-desktop.enable = true;
+
+  };
+
+  # Enable nix flakes
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  system.stateVersion = "22.05";
+
+  environment.systemPackages = [ pkgs.git pkgs.fzf neovim pkgs.fish ];
+}
